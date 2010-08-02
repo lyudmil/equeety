86689cc3887013f5606080401328d53218878aa1
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-08-02 15:51:37 +0200

    Initial commit -- just a barebones rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equeety::Application.config.secret_token = '8548994f61c7c762874b4fc40332729a0dba32e4836ec0c4914a3fb09e25765cb50f097b65d3b72228e7594ba37cd6ca2ce0f8db3188c3c6a7b47324e020ea35'
