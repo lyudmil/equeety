a79d846497769da2cbdc4d5498133cb8ddf624df
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-08-02 16:46:00 +0200

    Simple user sign up using authlogic. Still a lot more polishing to do.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      redirect_to('/', :notice => 'User was successfully created.')
+    else
+      render :action => "new"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Equeety::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
