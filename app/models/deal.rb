10692cdf1ac6d48e8cc0c3d4dda43756fcbac913
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-08-05 16:38:12 +0300

    Starting validation on deals. Most of these are to prevent SQL exceptions.

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,3 +1,6 @@
 class Deal < ActiveRecord::Base
   belongs_to :user
+  validates_numericality_of :required_amount, :greater_than => 0, :less_than => 10000000
+  validates_numericality_of :proposed_valuation, :greater_than => 0, :less_than => 1000000000
+  validates_format_of :contact_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -1,9 +1,79 @@
 require 'spec_helper'
 
-describe Deal do
+describe Deal do  
   it "should belong to a user" do
     user = mock_model('User')
     deal = Deal.new(:user => user)
     deal.user.should == user
+  end
+  
+  describe "required amount" do
+    it "should be numeric" do
+      assert_validates_numericality_of :required_amount
+    end
+    
+    it "should be positive" do
+      assert_validates_positiveness_of :required_amount
+    end
+    
+    it "should be less than $10,000,000" do
+      assert_validates_upper_bound_of :required_amount, 10000000
+    end
+  end
+  
+  describe "proposed valuation" do
+    it "should be numeric" do
+      assert_validates_numericality_of :proposed_valuation
+    end
+    
+    it "should be positive" do
+      assert_validates_positiveness_of :proposed_valuation
+    end
+    
+    it "should be less than $1,000,000,000" do
+      assert_validates_upper_bound_of :proposed_valuation, 1000000000
+    end
+  end
+  
+  it "should validate contact email address" do
+    deal = Deal.new(valid_fields.except(:contact_email))
+    deal.contact_email = 'some.random.string'
+    deal.should_not be_valid
+    deal.errors_on(:contact_email).should == ["is invalid"]
+    
+    validation_sanity_check
+  end
+  
+  def assert_validates_numericality_of field
+    deal = Deal.new(valid_fields.except(field))
+    deal.should_not be_valid
+    deal.errors_on(field).should == ["is not a number"]
+    
+    validation_sanity_check
+  end
+  
+  def assert_validates_positiveness_of field
+    deal = Deal.new(valid_fields.except(field).merge({field => 0}))
+    deal.should_not be_valid
+    deal.errors_on(field).should == ["must be greater than 0"]
+    
+    validation_sanity_check
+  end
+  
+  def assert_validates_upper_bound_of field, upper_bound
+    deal = Deal.new(valid_fields.except(field).merge({field => upper_bound}))
+    deal.should_not be_valid
+    deal.errors_on(field).should == ["must be less than #{upper_bound}"]
+    
+    validation_sanity_check
+  end
+  
+  def valid_fields
+    {:required_amount => 111, :proposed_valuation => 222, :contact_email => 'me@email.com'}
+  end
+  
+  def validation_sanity_check
+     deal = Deal.new(valid_fields)
+     deal.should be_valid
   end
 end
