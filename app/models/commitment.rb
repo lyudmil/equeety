dba8115859dd5659ce398091d07a6cfbb4427f92
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2010-09-30 05:39:21 +0200

    Making sure an investment cannot put an investor over budget. I'm afraid the testing here is a bit flaky - it's a difficult validation.

--- app/models/commitment.rb
+++ app/models/commitment.rb
@@ -1,7 +1,10 @@
+require 'user_budget_validator'
+
 class Commitment < ActiveRecord::Base
   belongs_to :user
   belongs_to :deal
   
   validates_numericality_of :amount, :greater_than => 0, :less_than => 1000000000
   validates_uniqueness_of :user_id, :scope => :deal_id, :message => "You can only invest in a deal once."
+  validates_with UserBudgetValidator
 end
