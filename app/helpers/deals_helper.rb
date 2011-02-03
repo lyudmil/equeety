2a937ae5498a17839f9b9e263e260917d5e3ca2d
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2011-02-03 02:17:18 +0100

    Changing the status image depending on the deal status.

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,9 +1,21 @@
 module DealsHelper
+  @@status_images = {
+    'new' => '/images/new.gif', 
+    'pending' => '/images/pending.gif',
+    'due_diligence' => '/images/due_delegence.gif',
+    'agreed' => '/images/agreed.gif',
+    'rejected' => '/images/rejected.gif'
+  }
+  
   def round_options
     ['Pre-Seed', 'Seed', 'A', 'B', 'C']
   end
   
   def status_options
     ['new', 'pending', 'due_diligence', 'agreed', 'rejected']
+  end
+  
+  def status_image_for deal
+    @@status_images[deal.status]
   end
 end
