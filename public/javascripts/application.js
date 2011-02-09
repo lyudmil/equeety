ffc2d0bba7203efb491d8bc8829abb3dccb84563
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2011-02-09 20:50:52 +0100

    Resizing logos on deal view page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function resize(elem) {
+	if (elem.width > elem.height) {
+		if (elem.width > 100) {
+			elem.width = 100;
+		}
+	} else {
+		if (elem.height > 100) {
+			elem.height = 100;
+		}
+	}
+}
+
+document.observe("dom:loaded", function() {
+	$$('img.logo_img').each(function (image) {
+		$(image).observe('load', function () {
+			resize(image);
+		})
+	})
+});
