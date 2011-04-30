73ac69822ad7792e3a6784c79df7117e28690736
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2011-04-30 18:39:29 +0200

    Removing a useless notice and making the progress bar work.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     @session = UserSession.new(params[:session])
     
     if @session.save
-      redirect_to dashboard_path, :notice => 'You\'ve logged in successfully.'
+      redirect_to dashboard_path
     else
       flash[:error] = 'Sorry, we couldn\'t match your email and password.'
       redirect_to login_path
