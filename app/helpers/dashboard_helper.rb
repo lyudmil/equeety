822e4fc257d5977caf8ece5a6fffcac178c63aa4
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2011-04-30 22:32:46 +0200

    Fixing dashboard page when no budget set.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,11 @@
 module DashboardHelper
   def remaining_budget_percentage
+    return 0 unless current_user.budget
     (current_user.remaining_budget / current_user.budget) * 100
+  end
+  
+  def progress_bar_message
+    return "You haven't set a budget yet. You can do so by changing your profile settings." unless current_user.budget
+    remaining_budget_percentage.to_s + "% of " + number_to_currency(current_user.budget, :units => "$") + " remaining." 
   end
 end
