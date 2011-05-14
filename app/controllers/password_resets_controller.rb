2ea3e1136aa8c4f5f4268d44d454b3eb59d5944a
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2011-05-14 17:44:35 +0200

    Not allowing users that are logged in to reset passwords.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,6 @@
 class PasswordResetsController < ApplicationController
   
+  before_filter :require_no_user
   before_filter :load_user_with_given_perishable_token
   
   def edit
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -4,10 +4,8 @@ describe PasswordResetsController do
   
   before :each do
     @user = mock_model(User, :perishable_token => "RW78z", :update_attributes => true, :reset_perishable_token! => true)
-    User.should_receive(:find_using_perishable_token).with("RW78z").and_return(@user)
+    User.stub(:find_using_perishable_token).with("RW78z").and_return(@user)
   end
-  
-  it "should require the user to be logged out"
   
   it "should display message if user not found"
   
@@ -20,6 +18,14 @@ describe PasswordResetsController do
       get :edit, :id => @user.perishable_token
       
       assigns(:user).should == @user
+    end
+    
+    it "should not allow logged in users" do
+      controller.should_receive(:current_user).and_return(mock_model(User))
+      
+      get :edit, :id => @user.perishable_token
+      
+      response.should redirect_to root_path
     end
   end
   
@@ -52,6 +58,14 @@ describe PasswordResetsController do
       put :update, :id => @user.perishable_token
       
       response.should render_template 'edit'
+    end
+    
+    it "should not allow logged in users" do
+      controller.stub(:current_user).and_return(mock_model(User))
+      
+      put :update, :id => @user.perishable_token
+      
+      response.should redirect_to root_path
     end
     
   end
